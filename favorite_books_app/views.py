3bc640382be33c5feeb21009f08c211cf9fedd03
tareqjj <74101098+tareqjj@users.noreply.github.com>
2020-12-19 23:27:15 +0200

    Update views.py

--- favorite_books_app/views.py
+++ favorite_books_app/views.py
@@ -15,7 +15,7 @@ def add_user(request):
     else:
         logged_user = models.reg_user(request.POST)
         request.session['logged_user_info'] = logged_user
-        return redirect('/display_books')
+        return redirect('/wall')
 
 def login(request):
     errors = models.User.objects.login_validator(request.POST)
@@ -27,50 +27,31 @@ def login(request):
         logged_user = models.login_user(request.POST)
         if logged_user:
             request.session['logged_user_info'] = logged_user
-            return redirect('/display_books')
+            return redirect('/wall')
         return redirect("/")
 
 def logout(request):
     del request.session['logged_user_info']
     return redirect('/')
 
-def add_book(request):
-    errors = models.Book.objects.book_validator(request.POST)
-    if len(errors) > 0:
-        for key, value in errors.items():
-            messages.error(request, value)
-        return redirect('/display_books')
-    else:
-        models.add_book_DB(request.POST, request.session['logged_user_info']['user_id'])
-        return redirect('/display_books')
+def post_message(request):
+    models.add_message(request.session['logged_user_info']['user_id'], request.POST)
+    return redirect('/wall')
 
-def add_toFavorite(request, book_id):
-    models.add_book_toFavorite(book_id, request.session['logged_user_info']['user_id'])
-    return redirect('/books/' + str(book_id))
+def post_comment(request):
+    models.add_comment(request.session['logged_user_info']['user_id'], request.POST)
+    return redirect('/wall')
 
-def remove_fromFavorite(request, book_id):
-    models.remove_book_fromFavorite(book_id, request.session['logged_user_info']['user_id'])
-    return redirect('/books/' + str(book_id))
 
-def edit_book(request, book_id):
-    models.editBook(book_id, request.POST)
-    return redirect('/books/' + str(book_id))
-
-def delete_book(request, book_id):
-    models.remove_Book(book_id)
-    return redirect('/display_books')
-
-def display_bookInfo(request, book_id):
-    context = models.view_bookInfo(book_id, request.session['logged_user_info']['user_id'])
-    return render(request, 'book_info.html', context)
-
-def display_books(request):
+def display_wall(request):
     if 'logged_user_info' in request.session:
-        context = models.view_books(request.session['logged_user_info']['user_id'])
-        return render(request, 'display_and_add_books.html', context)
+        context = models.view_wall()
+        return render(request, 'wall_main.html', context)
     else:
         return redirect("/")
 
-def display_userPage(request):
-    context = models.view_userPage(request.session['logged_user_info']['user_id'])
-    return render(request, 'user_page.html', context)
+def delete_message(request, user_id, message_id):
+    if user_id == request.session['logged_user_info']['user_id']:
+        models.remove_message(message_id)
+    return redirect('/wall')
+
